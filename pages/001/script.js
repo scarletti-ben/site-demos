fc011a5c6b042d0fac5ef6bdcfdbe0062c5f83e4
Ben Scarletti <scarletti.ben@gmail.com>
2025-07-31 15:06:06 +0100

    added new test demo

---
+++ pages/001/script.js
@@ -0,0 +1,10 @@
+// ~ ======================================================
+// ~ Entry Point
+// ~ ======================================================
+
+// ? Run callback when all resources have loaded
+window.addEventListener('load', () => {
+
+    console.log('testing');
+
+});
